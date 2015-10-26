3d7328d034a4a30a18b12f62b118465620102d1b
gatta <gatta@gattas-MacBook-Air.fritz.box>
2015-10-26 17:46:19 +1300

    update server to serve new data

--- js/serve/js/controller.js
+++ js/serve/js/controller.js
@@ -11,31 +11,52 @@ var apiReturn = function(response, err, data){
 
 exports.run = function(db, server){
 
-	server.registerAction('/api/shows/all', function(request, response, params){
-		db.all('SELECT s.*, e.* FROM show s LEFT JOIN next_unreleased_episode e ON (e.show_id = s.id) limit 5;', function(err, rows){
-			apiReturn(response, err, rows); 
-		});
-	});
+	// server.registerAction('/api/shows/all', function(request, response, params){
+	// 	db.all('SELECT s.*, e.* FROM show s LEFT JOIN next_unreleased_episode e ON (e.show_id = s.id) limit 5;', function(err, rows){
+	// 		apiReturn(response, err, rows); 
+	// 	});
+	// });
 
-	server.registerAction('/api/shows/find', function(request, response, params){
-		var like = '%' + params.query + '%';
-		var stmt = db.prepare('SELECT * FROM show WHERE name LIKE ? LIMIT 10;');
-		stmt.all(like, function(err, rows){
-			apiReturn(response, err, rows);
-		});
-		stmt.finalize();
-	});
+	// server.registerAction('/api/shows/find', function(request, response, params){
+	// 	var like = '%' + params.query + '%';
+	// 	var stmt = db.prepare('SELECT * FROM show WHERE name LIKE ? LIMIT 10;');
+	// 	stmt.all(like, function(err, rows){
+	// 		apiReturn(response, err, rows);
+	// 	});
+	// 	stmt.finalize();
+	// });
 
-	server.registerAction('/api/episodes/get', function(request, response, params){
-		var show_id = params.show_id;
-		var stmt = db.prepare('SELECT * FROM episode WHERE show_id = ?;');
-		stmt.all(show_id, function(err, rows){
-			apiReturn(response, err, rows);
-		});
-		stmt.finalize();
-	});
+	// server.registerAction('/api/episodes/get', function(request, response, params){
+	// 	var show_id = params.show_id;
+	// 	var stmt = db.prepare('SELECT * FROM episode WHERE show_id = ?;');
+	// 	stmt.all(show_id, function(err, rows){
+	// 		apiReturn(response, err, rows);
+	// 	});
+	// 	stmt.finalize();
+	// });
 
-	server.registerAction('/api/shows/returning', function(request, response, params){
+	// server.registerAction('/api/shows/returning/popular', function(request, response, params){
+	// 	var MAX_COUNT = 100;
+	// 	var start = params.start ? params.start : 0;
+	// 	var limit = params.limit ? params.limit : MAX_COUNT;
+	// 	if(limit > MAX_COUNT){
+	// 		apiReturn(response, 'Max count is '+MAX_COUNT);
+	// 		return;
+	// 	}
+	// 	var stmt = db.prepare(
+	// 		'SELECT show.id AS show_id, show.name AS show_name, season_number, release_date_raw, release_date_timestamp '+
+	// 		'FROM season_release '+
+	// 		'LEFT JOIN show ON (show.id = season_release.show_id) '+
+	// 		'WHERE season_release.release_date_timestamp > cast(strftime(\'%s\', \'now\') AS INTEGER) AND season_release.season_number > 1 '+
+	// 		'ORDER BY season_release.release_date_timestamp, release_date_raw ASC '+
+	// 		'LIMIT ? OFFSET ?;'
+	// 	);
+	// 	stmt.all(limit, start, function(err, rows){
+	// 		apiReturn(response, err, rows);
+	// 	});
+	// });
+
+	server.registerAction('/api/shows/returning/all', function(request, response, params){
 		var MAX_COUNT = 100;
 		var start = params.start ? params.start : 0;
 		var limit = params.limit ? params.limit : MAX_COUNT;
@@ -44,9 +65,10 @@ exports.run = function(db, server){
 			return;
 		}
 		var stmt = db.prepare(
-			'SELECT show.id AS show_id, show.name AS show_name, season_number, release_date_raw, release_date_timestamp '+
+			'SELECT show.id AS show_id, show.name AS show_name, season_number, release_date_raw, release_date_timestamp, tmdb_id, tmdb_poster_path '+
 			'FROM season_release '+
 			'LEFT JOIN show ON (show.id = season_release.show_id) '+
+			'LEFT JOIN show_extra ON (show.id = show_extra.show_id) '+
 			'WHERE season_release.release_date_timestamp > cast(strftime(\'%s\', \'now\') AS INTEGER) AND season_release.season_number > 1 '+
 			'ORDER BY season_release.release_date_timestamp, release_date_raw ASC '+
 			'LIMIT ? OFFSET ?;'
