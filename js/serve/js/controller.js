67c8d5df7ad7c0381cd1c9ee4673efa06be18063
gatta <gatta@gattas-MacBook-Air.fritz.box>
2015-10-26 19:31:27 +1300

    add popular shows api

--- js/serve/js/controller.js
+++ js/serve/js/controller.js
@@ -35,26 +35,28 @@ exports.run = function(db, server){
 	// 	stmt.finalize();
 	// });
 
-	// server.registerAction('/api/shows/returning/popular', function(request, response, params){
-	// 	var MAX_COUNT = 100;
-	// 	var start = params.start ? params.start : 0;
-	// 	var limit = params.limit ? params.limit : MAX_COUNT;
-	// 	if(limit > MAX_COUNT){
-	// 		apiReturn(response, 'Max count is '+MAX_COUNT);
-	// 		return;
-	// 	}
-	// 	var stmt = db.prepare(
-	// 		'SELECT show.id AS show_id, show.name AS show_name, season_number, release_date_raw, release_date_timestamp '+
-	// 		'FROM season_release '+
-	// 		'LEFT JOIN show ON (show.id = season_release.show_id) '+
-	// 		'WHERE season_release.release_date_timestamp > cast(strftime(\'%s\', \'now\') AS INTEGER) AND season_release.season_number > 1 '+
-	// 		'ORDER BY season_release.release_date_timestamp, release_date_raw ASC '+
-	// 		'LIMIT ? OFFSET ?;'
-	// 	);
-	// 	stmt.all(limit, start, function(err, rows){
-	// 		apiReturn(response, err, rows);
-	// 	});
-	// });
+	server.registerAction('/api/shows/returning/popular', function(request, response, params){
+		var MAX_COUNT = 100;
+		const MIN_POPULARITY = 0.1;
+		var start = params.start ? params.start : 0;
+		var limit = params.limit ? params.limit : MAX_COUNT;
+		if(limit > MAX_COUNT){
+			apiReturn(response, 'Max count is '+MAX_COUNT);
+			return;
+		}
+		var stmt = db.prepare(
+			'SELECT show.id AS show_id, show.name AS show_name, season_number, release_date_raw, release_date_timestamp, tmdb_id, tmdb_poster_path '+
+			'FROM season_release '+
+			'LEFT JOIN show ON (show.id = season_release.show_id) '+
+			'LEFT JOIN show_extra ON (show.id = show_extra.show_id) '+
+			'WHERE season_release.release_date_timestamp > cast(strftime(\'%s\', \'now\') AS INTEGER) AND season_release.season_number > 1 AND tmdb_popularity > '+MIN_POPULARITY+' '+
+			'ORDER BY season_release.release_date_timestamp, release_date_raw ASC '+
+			'LIMIT ? OFFSET ?;'
+		);
+		stmt.all(limit, start, function(err, rows){
+			apiReturn(response, err, rows);
+		});
+	});
 
 	server.registerAction('/api/shows/returning/all', function(request, response, params){
 		var MAX_COUNT = 100;
